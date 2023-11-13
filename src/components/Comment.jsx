2eea912ba8ef88fbf3731f57b79d78bb2663f552
Israel Tavares <israelbulcaotavares@gmail.com>
2023-11-12 23:08:54 -0300

    realizando like nos comentarios

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,12 +2,18 @@
 import { ThumbsUp, Trash } from '@phosphor-icons/react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
+import { useState } from 'react'
 
 //export function Comment(props) {
 export function Comment({ content, onDeleteComment }) {
+    const [likeCount, setLikeCount] = useState(0) /* tipo de dado numerico, já que será com contador de likes */
 
-    function handleDeleteComment() { 
+    function handleDeleteComment() {
         onDeleteComment(content)
+    }
+
+    function handleLikeComment(){
+        setLikeCount(likeCount + 1)
     }
 
     return (
@@ -27,9 +33,10 @@ export function Comment({ content, onDeleteComment }) {
                     <p>{content}</p>
                 </div>
                 <footer>
-                    <button>
+                    {/* <button onClick={() => setLikeCount(likeCount + 1)}> */}
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
